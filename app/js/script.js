8904b7de935b8ab9ddcf3b9e003cc947230cf34e
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-10-17 20:18:08 +0100

    Header & navigation bar for desktop

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,9 @@
 const features = document.querySelector('.features')
+const featuresDesktop = document.querySelector(".features-desktop");
 const dropdown = document.querySelector('.dropdown')
+const dropdownDesktop = document.querySelector(".drop-down-desktop");
 const arrow = document.querySelector('.arrow-down')
-const arrowcom = document.querySelector(".arrow-down-com");
+const arrowdesktop = document.querySelector(".arrow-desktop");
 
 features.addEventListener('click', function () {
     features.classList.toggle('add')
@@ -14,11 +16,39 @@ features.addEventListener('click', function () {
     }
 })
 
+featuresDesktop.addEventListener("click", function () {
+    featuresDesktop.classList.toggle("add");
+    dropdownDesktop.classList.toggle("active");
+
+    if (featuresDesktop.classList.contains("add")) {
+        arrowdesktop.src = "/images/icon-arrow-up.svg";
+
+    } else {
+        arrowdesktop.src = "/images/icon-arrow-down.svg";
+    }
+});
+
 const company = document.querySelector('.company')
+const companyDesktop = document.querySelector('.company-desktop')
 const dropdowncompany = document.querySelector(".dropdowncompany");
+const dropdowncompanyDesktop = document.querySelector(".dropdowncompany-desk");
+const arrowcom = document.querySelector(".arrow-down-com");
+const arrowcomDesk = document.querySelector(".arrow-desk");
+
+companyDesktop.addEventListener("click", function () {
+    companyDesktop.classList.toggle('daa')
+    dropdowncompanyDesktop.classList.toggle("active");
+
+    if (companyDesktop.classList.contains("daa")) {
+        arrowcomDesk.src = "/images/icon-arrow-up.svg";
+        
+    } else {
+        arrowcomDesk.src = "/images/icon-arrow-down.svg";
+    }
+});
 
 company.addEventListener("click", function () {
-    company.classList.toggle('daa')
+    company.classList.toggle("daa");
     dropdowncompany.classList.toggle("active");
 
     if (company.classList.contains("daa")) {
